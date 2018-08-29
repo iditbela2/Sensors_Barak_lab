b8fdb858f62494b10c57af0355f7ad8eee95788d
idit belachsen <iditbela2@gmail.com>
2018-08-29 09:48:38 +0300

    adding original zoeys files and the changes files

--- my_files/dataAnalysis.py
+++ my_files/dataAnalysis.py
@@ -1,16 +1,74 @@
 
 import matplotlib.pyplot as plt
+import datetime
+import os
+import numpy as np
+import time
+import dropbox
 
 # Make sure data was manually downloaded
 
-
-
-# read data from dropbox folder
-
-
-
 # the name of the file should represent the time at the end of the measurement, so use linespace with
 # the number of values to add x-time array
+
+sensor_no = 1
+start_date = datetime.datetime(2018,8,1,0,0,0)
+end_date = datetime.datetime(2018,8,2,0,0,0)
+duration = 5 # measurement duration
+dataPath = '/Users/iditbela/Documents/Sensors_Barak_lab/downloaded_data/'
+
+
+# find the log files that match the chosen dates and create data array of time intervals
+# and measurements. each measurement represents one minute so the time of the log-file is the end of
+# measurement and I go the number of required minutes back
+
+def getData(sensor, start, end):
+    file_names = np.array(os.listdir(dataPath+str(sensor)))
+    times_str = np.array([ind[4:] for ind in file_names])
+    fmt = "%Y-%m-%d %H:%M:%S"
+    times_datetime = np.array([datetime.datetime.strptime(times_str[x], fmt) for x in range(len(times_str))])
+    chosen_datetimes = times_datetime[(times_datetime >= start_date) & (times_datetime <= end_date)]
+    chosen_logs =
+
+
+
+
+
+
+
+
+
+    import time, glob
+
+    outfilename = 'all_' + str((int(time.time()))) + ".txt"
+
+    filenames = glob.glob('*.txt')
+
+    with open(outfilename, 'wb') as outfile:
+        for fname in filenames:
+            with open(fname, 'r') as readfile:
+                infile = readfile.read()
+                for line in infile:
+                    outfile.write(line)
+                outfile.write("\n\n")
+
+
+
+
+    path = r'C:\Users\Data1\\'
+    times = []
+    yvals = []
+    for data_file in sorted(os.listdir(path)):
+        with open(data_file, 'r') as f:
+            for line in f.readlines()[11:21]:  # read lines from 11 to 21
+                column = line.split('\t')
+                times.append(column[0])
+                yvals.append(column[1])
+
+
+
+
+# convert all name of files to time and then concat to one file / build an array based on start and end
 
 
 
