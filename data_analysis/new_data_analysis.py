3e98785b62b8745ba3fbd0f94c25a7bd127cba89
idit belachsen <iditbela2@gmail.com>
2018-10-15 14:58:19 +0300

    notebook after some small games

--- data_analysis/new_data_analysis.py
+++ data_analysis/new_data_analysis.py
@@ -1,4 +1,96 @@
 
+# BACKUP OF JUPYTER NOTEBOOK
+
+# import relevant modules
+import data_analysis.DA_functions as da
+import numpy as np
+import datetime
+import csv
+import matplotlib.pyplot as plt
+import matplotlib.dates as md
+
+
+# set path and choose time of measurement
+dataPath = '/Users/iditbela/Dropbox/'
+start_date = datetime.datetime(2018, 9, 26, 0, 0, 0)
+end_date = datetime.datetime(2018, 10, 2, 10, 0, 0)
+duration = 10 # measurement duration
+
+# SENSOR-1-SDS021
+sensor_no = 1
+output_no = 2
+times_chosen, times_log = da.getTimes(dataPath,sensor_no,start_date,end_date,log = 'data')
+times_1, data_1 = da.getSignalData(dataPath, sensor_no,start_date, end_date, duration, output_no)
+
+# from IPython.core.interactiveshell import InteractiveShell
+# InteractiveShell.ast_node_interactivity = "all"
+
+#times_1
+#data_1
+
+#%matplotlib notebook
+# plot signal SENSOR-1-SDS021
+fmt = "%Y-%m-%d %H:%M:%S"
+#plt.style.use('ggplot')
+fig, ax = plt.subplots()
+ax.plot(times_1, data_1[:,0])
+ax.plot(times_1,data_1[:,1])
+ax.set_ylim(0,80)
+ax.xaxis.set_major_formatter(md.DateFormatter("%Y-%m-%d"))
+ax.legend(('PM 2.5','PM 10'),loc='upper right')
+plt.title('PM concentration\n measured by sensor '+str(sensor_no)+'\nbetween '+times_1[0].strftime(fmt)+' and '+ (times_1[len(times_1)-1]+datetime.timedelta(hours=0, minutes=1)).strftime(fmt))
+plt.ylabel(r'$\mu g/m^3$',labelpad=16)
+# Tell matplotlib to interpret the x-axis values as dates
+ax.xaxis_date()
+# Make space for and rotate the x-axis tick labels
+fig.autofmt_xdate()
+plt.rcParams['figure.figsize'] = (20, 16)
+plt.rcParams['font.size'] = 20
+
+
+plt.close('all')
+plt.rcParams.update(plt.rcParamsDefault)
+
+
+# SENSOR-2-PMS5003
+sensor_no = 2
+output_no = 12
+times_chosen, times_log = da.getTimes(dataPath,sensor_no,start_date,end_date,log = 'data')
+times_2, data_2 = da.getSignalData(dataPath, sensor_no,start_date, end_date, duration, output_no)
+
+
+%matplotlib notebook
+# plot signal SENSOR-2-PMS5003
+fmt = "%Y-%m-%d %H:%M:%S"
+#plt.style.use('ggplot')
+fig, ax = plt.subplots()
+for i in range(6):
+    ax.plot(times_2, data_2[:,i])
+ax.xaxis.set_major_formatter(md.DateFormatter("%Y-%m-%d"))
+# plot concentration
+ax.legend(('pm1', 'pm2.5', 'pm10', 'pm1atm', 'pm2.5atm', 'pm10atm'),loc='upper right')
+plt.title('PM concentration\n measured by sensor '+str(sensor_no)+'\nbetween '+times_2[0].strftime(fmt)+' and '+ (times_2[len(times_2)-1]+datetime.timedelta(hours=0, minutes=1)).strftime(fmt))
+plt.ylabel(r'$\mu g/m^3$',labelpad=16)
+# Tell matplotlib to interpret the x-axis values as dates
+ax.xaxis_date()
+# Make space for and rotate the x-axis tick labels
+fig.autofmt_xdate()
+plt.rcParams['figure.figsize'] = (20, 16)
+plt.rcParams['font.size'] = 20
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # SENSOR-1-SDS021
 sensor_no = 1
 start_date = datetime.datetime(2018, 9, 26, 0, 0, 0)
